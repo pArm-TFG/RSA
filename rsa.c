df27a73e184fe8483172cb049ab486a74d315617
Javinator9889 <dev@javinator9889.com>
2020-09-02 19:43:49 +0200

    Created new implementation of RSA

--- rsa.c
+++ rsa.c
@@ -1,313 +1,131 @@
+/* 
+ * File: rsa.c
+ * Author: Javinator9889
+ * Comments: RSA file with necessary functions
+ * Revision history: v1.0
+ */
+
+/*******************************************************************************
+    RSA Algorithm
+    Reminder: ((m**e)**d) % n = m
+    e: encryption, d: decryption
+    Encryption: ciphertext = message**e	% n
+    Decryption: (c**d == (m**e)**d == m )	% n
+
+    RSA Key Generation
+    p and q, two distrinct prime numbers
+    n = pq
+    fi is Euler's Totient Function
+    fi(n) = fi(p) * fi(q) = (p - 1) * (q - 1) = n - (p + q - 1)
+
+    chose e, the public key:
+        - 1 < e < fi(n)
+        - gcd(e, fi(n)) == 1 (i.e. e and fi(n) are coprime
+    chose d, the private key:
+        - d == e**-1 (mod fi(n))
+        -> d is the modular multiplicative inverse of e (modulo(fi(n)))
+ ******************************************************************************/
+
 #include <stdint.h>
-#include <string.h>
 #include <stdbool.h>
+#include "../utils/rand.h"
 #include "rsa.h"
 
-bool rsa1024(uint64_t res[], uint64_t data[], uint64_t expo[], uint64_t key[]) {
-    int32_t i, j, expo_len;
-    uint64_t mod_data[18] = {0}, result[18] = {0};
-    uint64_t temp_expo = 0;
 
-    modbignum(mod_data, data, key, 16);
-    result[0] = 1;
-    expo_len = bit_length(expo, 16) / 64;
-    for (i = 0; i < expo_len + 1; i++) {
-        temp_expo = expo[i];
-        for (j = 0; j < 64; j++) {
-            if (temp_expo & 0x1UL)
-                modmult1024(result, result, mod_data, key);
-
-            modmult1024(mod_data, mod_data, mod_data, key);
-            temp_expo = temp_expo >> 1;
-        }
-    }
-    for (i = 0; i < 16; i++)
-        res[i] = result[i];
-
-
-    return 1;
-}
-
-bool addbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n) {
-    uint32_t i;
-    uint64_t j, k, carry = 0;
-    for (i = 0; i < n; i++) {
-        j = (op1[i] & 0xffffffff) + (op2[i] & 0xffffffff) + carry;
-
-        k = ((op1[i] >> 32) & 0xffffffff) + ((op2[i] >> 32) & 0xffffffff) + ((j >> 32) & 0xffffffff);
-
-        carry = ((k >> 32) & 0xffffffff);
-
-        res[i] = ((k & 0xffffffff) << 32) | (j & 0xffffffff);
-    }
-    res[i] = carry;
-    return 0;
-}
-
-bool multbignum(uint64_t res[], uint64_t op1[], uint32_t op2, uint32_t n) {
-    uint32_t i;
-    uint64_t j, k, carry1 = 0, carry2 = 0;
-    for (i = 0; i < n; i++) {
-        j = (op1[i] & 0xffffffff) * (op2 & 0xffffffff);
-
-        k = ((op1[i] >> 32) & 0xffffffff) * (op2 & 0xffffffff);
-        carry1 = ((k >> 32) & 0xffffffff);
-        k = (k & 0xffffffff) + ((j >> 32) & 0xffffffff);
-        j = (j & 0xffffffff) + carry2;
-        k = k + ((j >> 32) & 0xffffffff);
-        carry2 = carry1 + ((k >> 32) & 0xffffffff);
-
-        res[i] = ((k & 0xffffffff) << 32) | (j & 0xffffffff);
-    }
-    res[i] = carry2;
-    return 0;
-}
-
-bool modmult1024(uint64_t res[], uint64_t op1[], uint64_t op2[], uint64_t mod[]) //optimized
-{
-    int32_t i, j;
-    uint64_t mult1[33] = {0}, mult2[33] = {0},
-    result[33] = {0}, xmod[33] = {0};
-
-    for (i = 0; i < 16; i++)
-        xmod[i] = mod[i];
-
-    for (i = 0; i < 16; i++) {
-        for (j = 0; j < 33; j++) {
-            mult1[j] = 0;
-            mult2[j] = 0;
-        }
-        multbignum(mult1, op1, (op2[i]&0xffffffff), 16);
-        multbignum(mult2, op1, ((op2[i] >> 32)&0xffffffff), 16);
-        slnbignum(mult2, mult2, 33, 32);
-        addbignum(mult2, mult2, mult1, 32);
-
-        slnbignum(mult2, mult2, 33, 64 * i);
-
-        addbignum(result, result, mult2, 32);
-
-    }
-    modbignum(result, result, xmod, 33);
-    for (i = 0; i < 16; i++)
-        res[i] = result[i];
-
-    return 0;
-}
-
-/*
-bool modmult1024(uint64_t res[], uint64_t op1[], uint64_t op2[],uint64_t mod[])
-{
-    int32_t i,j;
-    uint64_t mult1[19]={0},mult2[19]={0},result[18]={0};
-
-    for(i=0;i<16;i++)
-    {
-        multbignum(mult1,op1,(op2[i]&0xffffffff),16);
-
-        multbignum(mult2,op1,((op2[i]>>32)&0xffffffff),16);
-        slnbignum(mult2,mult2,17,32);
-        addbignum(mult2,mult2,mult1,17);
-        modbignum(mult2,mult2,mod,17);
-
-        for(j=0;j<i;j++)
-        {
-            slnbignum(mult2,mult2,17,64);
-            modbignum(mult2,mult2,mod,17);
-        }
-		
-        addbignum(result,result,mult2,16);
-        modbignum(result,result,mod,17);
-		
-    }
-    for(i=0;i<16;i++)
-            res[i]=result[i];
-	
-    return 0;
-}
- */
-bool modbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n)//optimized
-{
-    uint32_t i;
-    int32_t len_op1, len_op2, len_dif;
-
-    len_op1 = bit_length(op1, n);
-    len_op2 = bit_length(op2, n);
-    len_dif = len_op1 - len_op2;
+/*static uint64_t gcd(uint64_t a, uint64_t b) {
+    uint64_t q, r1, r2, r;
     
-//    VectorCopy(n, res, op1);
-
-    for (i = 0; i < n; i++)
-        res[i] = op1[i];
-
-    if (len_dif < 0) {
-        return 1;
+    if (a > b) {
+        r1 = a;
+        r2 = b;
+    } else {
+        r1 = b;
+        r2 = a;
     }
+    
+    while (r2 > 0) {
+        q = r1 / r2;
+        r = r1 - q * r2;
+        r1 = r2;
+        r2 = r;
+    }
+    
+    return r1;
+}*/
 
-    if (len_dif == 0) {
-        while (compare(res, op2, n) >= 0) {
-            subbignum(res, res, op2, n);
+static const uint64_t e = (uint64_t) ((2 << 15) + 1ULL);
+
+static uint64_t gcd(uint64_t u, uint64_t v) {
+    uint64_t shift;
+    if (u == 0ULL) return v;
+    if (v == 0ULL) return u;
+    
+    shift = __builtin_ctzll(u | v);
+    u >>= __builtin_ctzll(u);
+    
+    do {
+        v >>= __builtin_ctzll(v);
+        if (u > v) {
+            uint64_t temp = v;
+            v = u;
+            u = temp;
         }
-        return 1;
-    }
-
-    slnbignum(op2, op2, n, len_dif);
-    for (i = 0; i < len_dif; i++) {
-        srnbignum(op2, op2, n, 1);
-        while (compare(res, op2, n) >= 0) {
-            subbignum(res, res, op2, n);
-        }
-    }
-
-    return 1;
+        v -= u;
+    } while (v != 0ULL);
+    
+    return u << shift;
 }
 
-/****************************************************************
- * bool modnum(uint64_t res[],uint64_t op1[], uint64_t op2[],uint32_t n)
- * res = op1 % op2
- * n is bit length/64
- * res must have extra 64 bits to avoid errors 
- ****************************************************************/
-bool modnum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n) {
-    uint32_t i;
-    bool result = 0;
-    for (i = 0; i < n; i++)
-        res[i] = op1[i];
-
-    while (!result) {
-        result = subbignum(res, res, op2, n);
-    }
-
-    addbignum(res, res, op2, n);
-    res[n] = 0;
-
-    return 0;
+static inline bool check_valid_phi(uint64_t *phi) {
+    return gcd(e, *phi) != 1ULL;
 }
 
-/****************************************************************
- * int32_t compare(uint64_t op1[], uint64_t op2[],uint32_t n)
- * returns 1 if op1>op2
- * 		 -1 if op1<op2
- * 		  0 if op1=op2
- *****************************************************************/
-int32_t compare(uint64_t op1[], uint64_t op2[], uint32_t n) {
-    for (; n > 0; n--) {
-        if (op1[n - 1] > op2[n - 1]) {
-            return 1;
-        } else if (op1[n - 1] < op2[n - 1]) {
-            return -1;
-        }
+// Returns modulo inverse of a with respect 
+// to m using extended Euclid Algorithm 
+// Assumption: a and m are coprimes, i.e., 
+// gcd(a, m) = 1 
+static uint64_t mod_inverse(uint64_t a, uint64_t b) {
+    uint64_t m0 = b;
+    uint64_t x = 1;
+    uint64_t y = 0;
+    
+    if (b == 1)
+        return 0ULL;
+    
+    while (a > 1) {
+        uint64_t quotient = (uint64_t) (a / b);
+        uint64_t temp = b;
+        
+        b = a % b;
+        a = temp;
+        temp = y;
+        
+        y = x - quotient * y;
+        x = temp;
     }
-
-    return 0;
+    
+    if (x < 0)
+        x += m0;
+    
+    return x;
 }
 
-/****************************************************************
- * bool subbignum(uint64_t res[], uint64_t op1[], uint64_t op2[],uint32_t n)
- * subtracts op2 from op1
- * returns 0 if op1>=op2
- * 		   1 if op1<op2
- * result is not valid if return value is 1 (or is in 2's compliment :P)
- * **************************************************************/
-bool subbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n) {
-    bool carry = 0;
-    uint32_t i;
-    for (i = 0; i < n; i++) {
-        if (carry) {
-            if (op1[i] != 0)
-                carry = 0;
-            op1[i]--;
-        }
-        if (op1[i] < op2[i])
-            carry = 1;
-
-        res[i] = op1[i] - op2[i];
-    }
-    return carry;
-}
-
-bool slnbignum(uint64_t res[], uint64_t op[], uint32_t len, uint32_t n)//shift left by n
-{
-    uint32_t i, x, y;
-    uint64_t j, k, carry = 0;
-    x = n / 64;
-    y = n % 64;
-
-    for (i = len; i - x > 0; i--) {
-        res[i - 1] = op[i - 1 - x];
-    }
-    for (; i > 0; i--) {
-        res[i - 1] = 0;
-    }
-    for (i = 0; i < len; i++) {
-        j = res[i];
-        k = 0;
-        for (x = 0; x < y; x++) {
-            if (j & 0x8000000000000000) {
-                k = (k << 1) | 1;
-            } else {
-                k = (k << 1);
-            }
-            j = j << 1;
-        }
-        res[i] = j | carry;
-        carry = k;
-    }
-    return 1;
-}
-
-bool srnbignum(uint64_t res[], uint64_t op[], uint32_t len, uint32_t n)//shift right by n
-{
-    uint32_t i, x, y;
-    uint64_t j, k, carry = 0;
-    x = n / 64;
-    y = n % 64;
-
-    for (i = 0; i + x < len; i++) {
-        res[i] = op[i + x];
-    }
-    for (; i < len; i++) {
-        res[i] = 0;
-    }
-    for (i = len; i > 0; i--) {
-        j = res[i - 1];
-        k = 0;
-        for (x = 0; x < y; x++) {
-            if (j & 0x0000000000000001) {
-                k = (k >> 1) | 0x8000000000000000;
-            } else {
-                k = (k >> 1);
-            }
-            j = j >> 1;
-        }
-        res[i - 1] = j | carry;
-        carry = k;
-    }
-    return 1;
-
-}
-
-/****************************************************************
- * uint32_t bit_length(uint64_t op[],uint32_t n)
- * returns position of MSB present
- *
- *
- ****************************************************************/
-uint32_t bit_length(uint64_t op[], uint32_t n) {
-    uint32_t len = 0;
-    uint32_t i;
-    uint64_t unit = 1;
-    for (; n > 0; n--) {
-        if (op[n - 1] == 0)
-            continue;
-        for (i = 64; i > 0; i--) {
-            if (op[n - 1] & (unit << (i - 1))) {
-                len = (64 * (n - 1)) + i;
-                break;
-            }
-
-        }
-        if (len)
-            break;
-    }
-    return len;
+rsa_t RSA_keygen() {
+    rsa_t ret = { 0ULL };
+    uint32_t p;
+    uint32_t q;
+    
+    do {
+        p = (uint32_t) RAND(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER);
+    } while (p % 2 == 0 && p % e == 1);
+    
+    do {
+        q = (uint32_t) RAND(MIN_PRIME_NUMBER, MAX_PRIME_NUMBER);
+    } while (q % 2 == 0 && q % e == 1);
+    
+    ret.n = (p * q);
+    ret.phi = (p - 1) * (q - 1);
+    ret.d = mod_inverse(ret.e, ret.phi);
+    
+    return ret;
 }
--- rsa.h
+++ rsa.h
@@ -1,26 +1,78 @@
-/*
- * rsa.h
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
  *
- *  Created on: 09-May-2017
- *      Author: navin
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
  */
 
-#ifndef RSA_H_
-#define RSA_H_
+/* 
+ * File: rsa.h
+ * Author: Javinator9889
+ * Comments: RSA header file which common types and functions
+ * Revision history: v1.0
+ */
 
+/*******************************************************************************
+    RSA Algorithm
+    Reminder: ((m**e)**d) % n = m
+    e: encryption, d: decryption
+    Encryption: ciphertext = message**e	% n
+    Decryption: (c**d == (m**e)**d == m )	% n
+
+    RSA Key Generation
+    p and q, two distrinct prime numbers
+    n = pq
+    fi is Euler's Totient Function
+    fi(n) = fi(p) * fi(q) = (p - 1) * (q - 1) = n - (p + q - 1)
+
+    chose e, the public key:
+        - 1 < e < fi(n)
+        - gcd(e, fi(n)) == 1 (i.e. e and fi(n) are coprime
+    chose d, the private key:
+        - d == e**-1 (mod fi(n))
+        -> d is the modular multiplicative inverse of e (modulo(fi(n)))
+ ******************************************************************************/
+#ifndef RSA_H
+#define	RSA_H
+
+// Custom type definitions
 #include <stdint.h>
-#include <stdbool.h>
 
-bool addbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n);
-bool subbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n);
-bool modbignum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n);
-bool modnum(uint64_t res[], uint64_t op1[], uint64_t op2[], uint32_t n);
-bool modmult1024(uint64_t res[], uint64_t op1[], uint64_t op2[], uint64_t mod[]);
-bool rsa1024(uint64_t res[], uint64_t data[], uint64_t expo[], uint64_t key[]);
-bool multbignum(uint64_t res[], uint64_t op1[], uint32_t op2 , uint32_t n);
-uint32_t bit_length(uint64_t op[], uint32_t n);
-int32_t compare(uint64_t op1[], uint64_t op2[], uint32_t n);
-bool slnbignum(uint64_t res[], uint64_t op[], uint32_t len, uint32_t n); //shift left by n
-bool srnbignum(uint64_t res[], uint64_t op[], uint32_t len, uint32_t n);
+/**
+ * Limit to be used for calls to sieve algorithm if necessary
+ */
+#define RSA_SIEVE_LIMIT 200000ULL
 
-#endif /* RSA_H_ */
+/**
+ * Prime number limits
+ */
+#define MIN_PRIME_NUMBER 12049ULL
+#define MAX_PRIME_NUMBER 185621ULL
+
+typedef struct {
+    uint64_t n;
+    uint64_t phi;
+    uint64_t e;
+    uint64_t d;
+} rsa_t;
+
+rsa_t RSA_keygen(void);
+uint64_t RSA_encrypt(uint64_t *msg, rsa_t *key);
+uint64_t RSA_decrypt(uint64_t *text, rsa_t *key);
+
+#endif	/* RSA_H */
+
