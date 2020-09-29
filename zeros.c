79f8976cfcd2686ea9e1c113f20983f9dabda12d
Javinator9889 <dev@javinator9889.com>
2020-09-29 10:20:14 +0200

    Updated zeros.c for avoiding errors with reserved name 'base'

--- zeros.c
+++ zeros.c
@@ -20,35 +20,35 @@ const unsigned char clz_8b[256] = {
 };
 
 int_fast64_t clz(int_fast64_t x) {
-    int_fast64_t base, ms_oct;
+    int_fast64_t b, ms_oct;
 
     if (x & 0xFFFFFFFF00000000U)
         if (x & 0xFFFF000000000000U)
             if (x & 0xFF00000000000000U)
-                base = 0, ms_oct = x >> 56;
+                b = 0, ms_oct = x >> 56;
             else
-                base = 8, ms_oct = x >> 48;
+                b = 8, ms_oct = x >> 48;
         else
             if (x & 0x0000FF0000000000U)
-            base = 16, ms_oct = x >> 40;
+            b = 16, ms_oct = x >> 40;
         else
-            base = 24, ms_oct = x >> 32;
+            b = 24, ms_oct = x >> 32;
     else
         if (x & 0x00000000FFFF0000U)
         if (x & 0x00000000FF000000U)
-            base = 32, ms_oct = x >> 24;
+            b = 32, ms_oct = x >> 24;
         else
-            base = 40, ms_oct = x >> 16;
+            b = 40, ms_oct = x >> 16;
     else
         if (x & 0x000000000000FF00U)
-        base = 48, ms_oct = x >> 8;
+        b = 48, ms_oct = x >> 8;
     else
-        base = 56, ms_oct = x >> 0;
+        b = 56, ms_oct = x >> 0;
 
-    return base + clz_8b[ms_oct];
+    return b + clz_8b[ms_oct];
 }
 
 int_fast64_t ctz(int_fast64_t x) {
     x &= -x;
-    return 64LL - (clz(x) + 1);
+    return 63LL - clz(x);
 }
